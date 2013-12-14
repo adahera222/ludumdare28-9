20e1a546cc5b53cbedc8414d28e7b573f9a87cc3
zzorn <zzorn@iki.fi>
2013-12-14 13:14:38 +0200

    Added project pom file. Added stub mainclass.

---
+++ src/main/java/org/ludumdare28/GameMain.java
@@ -0,0 +1,13 @@
+package org.ludumdare28;
+
+/**
+ * Main class for the game.
+ */
+public class GameMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
